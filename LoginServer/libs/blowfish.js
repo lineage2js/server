ad4a0e4aeb98695671942f4a72e0ddf2c32c47e9
space2pacman <space2pacman@gmail.com>
2024-04-15 12:30:48 +0300

    refactoring blowfish

--- LoginServer/libs/blowfish.js
+++ LoginServer/libs/blowfish.js
@@ -1,9 +1,9 @@
 function BlowfishEngine(key) {
-  var KP = [0x243F6A88, 0x85A308D3, 0x13198A2E,
+  const KP = [0x243F6A88, 0x85A308D3, 0x13198A2E,
     0x03707344, 0xA4093822, 0x299F31D0, 0x082EFA98, 0xEC4E6C89,
     0x452821E6, 0x38D01377, 0xBE5466CF, 0x34E90C6C, 0xC0AC29B7,
     0xC97C50DD, 0x3F84D5B5, 0xB5470917, 0x9216D5D9, 0x8979FB1B];
-  var KS0 = [0xD1310BA6, 0x98DFB5AC, 0x2FFD72DB,
+  const KS0 = [0xD1310BA6, 0x98DFB5AC, 0x2FFD72DB,
     0xD01ADFB7, 0xB8E1AFED, 0x6A267E96, 0xBA7C9045, 0xF12C7F99,
     0x24A19947, 0xB3916CF7, 0x0801F2E2, 0x858EFC16, 0x636920D8,
     0x71574E69, 0xA458FEA3, 0xF4933D7E, 0x0D95748F, 0x728EB658,
@@ -55,7 +55,7 @@ function BlowfishEngine(key) {
     0x81E67400, 0x08BA6FB5, 0x571BE91F, 0xF296EC6B, 0x2A0DD915,
     0xB6636521, 0xE7B9F9B6, 0xFF34052E, 0xC5855664, 0x53B02D5D,
     0xA99F8FA1, 0x08BA4799, 0x6E85076A];
-  var KS1 = [0x4B7A70E9, 0xB5B32944, 0xDB75092E,
+  const KS1 = [0x4B7A70E9, 0xB5B32944, 0xDB75092E,
     0xC4192623, 0xAD6EA6B0, 0x49A7DF7D, 0x9CEE60B8, 0x8FEDB266,
     0xECAA8C71, 0x699A17FF, 0x5664526C, 0xC2B19EE1, 0x193602A5,
     0x75094C29, 0xA0591340, 0xE4183A3E, 0x3F54989A, 0x5B429D65,
@@ -107,7 +107,7 @@ function BlowfishEngine(key) {
     0x0E1E9EC9, 0xDB73DBD3, 0x105588CD, 0x675FDA79, 0xE3674340,
     0xC5C43465, 0x713E38D8, 0x3D28F89E, 0xF16DFF20, 0x153E21E7,
     0x8FB03D4A, 0xE6E39F2B, 0xDB83ADF7];
-  var KS2 = [0xE93D5A68, 0x948140F7, 0xF64C261C,
+  const KS2 = [0xE93D5A68, 0x948140F7, 0xF64C261C,
     0x94692934, 0x411520F7, 0x7602D4F7, 0xBCF46B2E, 0xD4A20068,
     0xD4082471, 0x3320F46A, 0x43B7D4B7, 0x500061AF, 0x1E39F62E,
     0x97244546, 0x14214F74, 0xBF8B8840, 0x4D95FC1D, 0x96B591AF,
@@ -159,7 +159,7 @@ function BlowfishEngine(key) {
     0x4DAD0FC4, 0x1E50EF5E, 0xB161E6F8, 0xA28514D9, 0x6C51133C,
     0x6FD5C7E7, 0x56E14EC4, 0x362ABFCE, 0xDDC6C837, 0xD79A3234,
     0x92638212, 0x670EFA8E, 0x406000E0];
-  var KS3 = [0x3A39CE37, 0xD3FAF5CF, 0xABC27737,
+  const KS3 = [0x3A39CE37, 0xD3FAF5CF, 0xABC27737,
     0x5AC52D1B, 0x5CB0679E, 0x4FA33742, 0xD3822740, 0x99BC9BBE,
     0xD5118E9D, 0xBF0F7315, 0xD62D1C7E, 0xC700C47B, 0xB78C1B6B,
     0x21A19045, 0xB26EB1BE, 0x6A366EB4, 0x5748AB2F, 0xBC946E79,
@@ -212,15 +212,15 @@ function BlowfishEngine(key) {
     0x90D4F869, 0xA65CDEA0, 0x3F09252D, 0xC208E69F, 0xB74E6132,
     0xCE77E25B, 0x578FDFE3, 0x3AC372E6];
 
-  var ROUNDS = 16;
-  var BLOCK_SIZE = 8; // bytes = 64 bits
-  var SBOX_SK = 256;
-  var P_SZ = ROUNDS + 2;
-  var S0 = Array(SBOX_SK);
-  var S1 = Array(SBOX_SK);
-  var S2 = Array(SBOX_SK);
-  var S3 = Array(SBOX_SK);
-  var P = Array(P_SZ);
+  const ROUNDS = 16;
+  const BLOCK_SIZE = 8; // bytes = 64 bits
+  const SBOX_SK = 256;
+  const P_SZ = ROUNDS + 2;
+  let S0 = Array(SBOX_SK);
+  let S1 = Array(SBOX_SK);
+  let S2 = Array(SBOX_SK);
+  let S3 = Array(SBOX_SK);
+  let P = Array(P_SZ);
 
   setKey(key);
   
@@ -251,12 +251,12 @@ function BlowfishEngine(key) {
   }
 
   function processTable(xl, xr, table) {
-    var size = table.length;
+    const size = table.length;
 
-    for (var s = 0; s < size; s += 2) {
+    for (let s = 0; s < size; s += 2) {
       xl ^= P[0];
 
-      for (var i = 1; i < ROUNDS; i += 2) {
+      for (let i = 1; i < ROUNDS; i += 2) {
         xr ^= F(xl) ^ P[i];
         xl ^= F(xr) ^ P[i + 1];
       }
@@ -276,13 +276,13 @@ function BlowfishEngine(key) {
     S3 = KS3.slice(0, SBOX_SK);
     P = KP.slice(0, P_SZ);
 
-    var keyLength = key.length;
-    var keyIndex = 0;
+    const keyLength = key.length;
+    let keyIndex = 0;
     
-    for (var i = 0; i < P_SZ; i++) {
-      var data = 0;
+    for (let i = 0; i < P_SZ; i++) {
+      let data = 0;
       
-      for (var j = 0; j < 4; j++) {
+      for (let j = 0; j < 4; j++) {
         data = (data << 8) | (key[keyIndex++] & 0xff);
 
         if (keyIndex > keyLength) {
@@ -301,8 +301,8 @@ function BlowfishEngine(key) {
   }
   
   function encryptBlock(src, srcIndex, dst, dstIndex) {
-    var xl = BytesTo32bits(src, srcIndex);
-    var xr = BytesTo32bits(src, srcIndex + 4);
+    let xl = BytesTo32bits(src, srcIndex);
+    let xr = BytesTo32bits(src, srcIndex + 4);
     
     xl ^= P[0];
 
@@ -317,12 +317,12 @@ function BlowfishEngine(key) {
   }
 
   function decryptBlock(src, srcIndex, dst, dstIndex) {
-    var xl = BytesTo32bits(src, srcIndex);
-    var xr = BytesTo32bits(src, srcIndex + 4);
+    let xl = BytesTo32bits(src, srcIndex);
+    let xr = BytesTo32bits(src, srcIndex + 4);
 
     xl ^= P[ROUNDS + 1];
 
-    for (var i = ROUNDS; i > 0; i -= 2) {
+    for (let i = ROUNDS; i > 0; i -= 2) {
       xr ^= F(xl) ^ P[i];
       xl ^= F(xr) ^ P[i - 1];
     }
