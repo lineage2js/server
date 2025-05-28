6681bd43dfefe14f5a0d051c7d946b39fb7f0bc3
space2pacman <space2pacman@gmail.com>
2025-05-29 01:55:36 +0300

    add Inventory components

---
+++ GameServer/Components/Inventory.js
@@ -0,0 +1,20 @@
+class Inventory {
+  constructor() {
+    this._items = [
+      {
+        location: 'bag',
+        item: {},
+      }
+    ];
+  }
+
+  addItem() {
+
+  }
+
+  getItem() {
+
+  }
+}
+
+module.exports = new Inventory;
