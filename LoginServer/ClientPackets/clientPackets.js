91159494d4546086d0c2dc981499595bce3b308b
space2pacman <space2pacman@gmail.com>
2025-06-27 13:04:15 +0300

    separate login server from gameserver

--- LoginServer/ClientPackets/clientPackets.js
+++
@@ -1,9 +0,0 @@
-const RequestAuthLogin = require('./RequestAuthLogin');
-const RequestServerList = require('./RequestServerList');
-const RequestServerLogin = require('./RequestServerLogin');
-
-module.exports = {
-  RequestAuthLogin,
-  RequestServerList,
-  RequestServerLogin,
-}
