39847ad554bde278b9499af857d4dd4752f64d00
space2pacman <space2pacman@gmail.com>
2025-07-02 14:08:26 +0300

    remove config

--- config/index.js
+++ config/index.js
@@ -1,11 +1,9 @@
 const main = require('./main');
-const loginserver = require('./loginserver');
 const gameserver = require('./gameserver');
 const database = require('./database');
 
 module.exports = {
   main,
-  loginserver,
   gameserver,
   database,
 }
